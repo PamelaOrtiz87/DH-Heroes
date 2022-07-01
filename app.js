e88321b30e58da0fb7be8d552d3426b39b1e7c5a
PamelaOrtiz87 <i.p.ortiz1987@gmail.com>
2022-06-30 23:20:54 -0300

    se crea rutas

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const port = 3030
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/home', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'index.html')))
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'index.html')))
+app.get('/babbage', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'babbage.html')))
+app.get('/berners-lee', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'berners-lee.html')))
+app.get('/clarke', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'clarke.html')))
+app.get('/hammilton', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'hammilton.html')))
+app.get('/hopper', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'hopper.html')))
+app.get('/lovelace', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'lovelace.html')))
+app.get('/turing', (req,res) => res.sendFile(path.resolve(__dirname,'views', 'turing.html')))
+
+
 
 app.listen(port,() => console.log('Se levantó con éxito el servidor en http://localhost:${port}'))
